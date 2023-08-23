fd0019d298c9c077f1075a80da1558b289c6eb78
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-23 15:36:44 -0600

    complete lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(true)
+
+  const appClass = inCart ? "in-cart" : ""
+  const toCart = inCart ? "Remove form cart" : "add to cart"
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={() => setInCart(!inCart)}>{toCart}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [filter, setFilter] = useState("All")
+  const filtered = items.filter((item) => {
+    if (filter === "All") {
+      return true
+    } return item.category === filter
+  })
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={(e) => setFilter(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
